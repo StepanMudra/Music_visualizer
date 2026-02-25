5c4b96870247793b8261e6d3993dd4e181a1f0ac
stepan <stepan.mudra@gmail.com>
2026-02-25 06:44:26 +0100

    Refactor EA

--- EmotionAnalyzer.py
+++ EmotionAnalyzer.py
@@ -1,24 +1,21 @@
+import torch
 from transformers import pipeline
 import librosa
 
 class EmotionAnalyzer:
 
-    def analyze_emotion(self, text):
+    def analyze_emotion(self, text, model):
         # Vytvoření pipeline pro analýzu emocí
-        emotion_classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base",
-                                      return_all_scores=True)
+        emotion_classifier = pipeline("text-classification", model=model, return_all_scores=True)
         # Analýza emocí
         emotions = emotion_classifier(text)
         return emotions
 
-    def analyze_music(self):
-        filename = 'filename'
+    def analyze_music(self, filename):
         y, sr = librosa.load(filename)
-
         # Extrakce základních hudebních rysů
         tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
         harmonic, percussive = librosa.effects.hpss(y)
         chromagram = librosa.feature.chroma_cqt(y=harmonic, sr=sr)
-
         # Výpis základních rysů
         return tempo, chromagram
