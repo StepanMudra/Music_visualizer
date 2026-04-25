cfba3d4829a81ea7165287405616d74a737ce0a1
stepan <stepan.mudra@gmail.com>
2026-04-25 22:44:49 +0200

    Mothods for making prompts

--- DescriptionMaker.py
+++ DescriptionMaker.py
@@ -1,101 +1,10 @@
 class DescriptionMaker:
 
-    def __init__(self):
+    def make_first_describer_prompt(self, template, emotion_result, music_result, text):
+        return template.format(emotions=emotion_result, music=music_result, text=text)
 
-analyze = analyzator.analyze_emotion(text)
+    def make_describer_prompt(self, template, description, feedback, emotion_result, music_result, text):
+        return template.format(description=description, feedback=feedback, emotions=emotion_result, music=music_result, text=text)
 
-prompt = f"""
-Generate a detailed description based on sentiment {analyze} where emotions are represented by colours 
-and probability of emotions represents amount of colour of the emotion,
-after it use tempo and chromagram for the atmosphere of the picture{analyze}.
-"""
-
-
-
-
-
-
-
-
-
-p = f"""#Identity:
-            You are an expert for reading emotion from music defined by chromagram and tempo. Also you are master for expresing emotions from music. Also you can put colours emotions You describing it for a painter who will paint an art based on your description.
-        #Task
-            Make description for a painter based on These emotions {analyze} and these music {analyzator.analyze_music()} and text {text}"""
-
-
-# Generování textového popisu
-response = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": p,
-        }
-    ],
-    model="gpt-4o",
-)
-
-print(response.choices[0].message.content)
-
-
-history = response.choices[0].message.content
-# Vytvoření promptu pro generování popisu
-prompt_image = f"""
-Generate an image based on description {response.choices[0].message.content}.
-"""
-
-# Generování textového popisu
-response_image = client.images.generate(
-    model="dall-e-3",
-    prompt=prompt_image,
-    size="1024x1024",
-    quality="standard",
-    n=1,
-)
-
-image_url = response_image.data[0].url
-print(image_url)
-k = 0
-while(k < 3):
-    pro = f"""#Identity:
-                    You are my critics and you should provide me guidance to better describing emotion for painting.
-              #Task:
-                    Look at these: {history}"""
-    response = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": pro,
-            }
-        ],
-        model="gpt-4o",
-    )
-    print(response.choices[0].message.content)
-    history = history + "Describtion of emotions: " +response.choices[0].message.content
-    if k == 4:
-        pro = f"""#Identity:
-                    You are an expert for expresing and describing emotions and you should listen to critic and learn form past and critic points.
-                   #Task:
-                    {history} max length is 4000 characters."""
-    else:
-        pro = f"""#Identity:
-                    You are an expert for expresing and describing emotions and you should listen to critic and learn form past and critic points.
-                   #Task:
-                    {history}"""
-    response = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": pro,
-            }
-        ],
-        model="gpt-4o",
-    )
-    print(response.choices[0].message.content)
-    history = history + "Critics: " +response.choices[0].message.content
-    k += 1
-
-# Vytvoření promptu pro generování popisu
-prompt_image = f"""
-Generate an image based on description {response.choices[0].message.content}.
-"""
+    def make_critic_prompt(self, template, description):
+        return template.format(description=description)
--- Engine.py
+++ Engine.py
@@ -8,35 +8,48 @@ class Engine:
         config_loader = Configuration_loader()
         emotion_analyzer = EmotionAnalyzer()
         description_maker = DescriptionMaker()
+
         text = config_loader.get_text
         path = config_loader.get_path
         key = config_loader.get_api_key
+        number_of_iterations = config_loader.get_number_of_iterations
         emotion_analysis_model = config_loader.get_emotion_analysis_model
+        text_model = config_loader.get_text_model
+        image_model = config_loader.get_image_model
+
+        describer_first_template = config_loader.get_describer_first
+        describer_template = config_loader.get_describer
+        critic_template = config_loader.get_critic
+
         text_analysis = emotion_analyzer.analyze_emotion(text, emotion_analysis_model)
-        #music_analysis= emotion_analyzer.analyze_music(path)
-        #api_handler = APIHandler(key)
-        describer = description_maker.make_description()
+        music_analysis= emotion_analyzer.analyze_music(path)
+
+        api_handler = APIHandler(key)
+
+        print("Text analysis")
         print(text_analysis)
+        print("Music analysis")
+        print(music_analysis)
+        print("Text")
+        print(text)
+        describer_first_prompt = description_maker.make_first_describer_prompt(describer_first_template, text_analysis, music_analysis, text)
+        i = 1
+        response_describer = api_handler.get_text_response(describer_first_prompt, text_model)
+        print("First describer response")
+        print(response_describer)
+        while i < int(number_of_iterations):
+            critic_prompt = description_maker.make_critic_prompt(critic_template, response_describer)
+            response_critic = api_handler.get_text_response(critic_prompt, text_model)
+            print("Response critic")
+            print(response_critic)
+            describer_prompt = description_maker.make_describer_prompt(describer_template, response_describer, response_critic, text_analysis, music_analysis, text)
+            response_describer = api_handler.get_text_response(describer_prompt, text_model)
+            print("Response describer")
+            print(response_describer)
+            i += 1
+
+        image_url = api_handler.get_image_response(response_describer, image_model)
+        print(image_url)
+
 Engine().run()
 
-
-# Text k analýze
-text = """
-text
-"""
-
-
-analyzator = EmotionAnalyzer()
-
-
-# Generování textového popisu
-# response_image = client.images.generate(
-#     model="dall-e-3",
-#     prompt=prompt_image,
-#     size="1024x1024",
-#     quality="standard",
-#     n=1,
-# )
-
-# image_url = response_image.data[0].url
-# print(image_url)
